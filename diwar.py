d726b9e783c09aa479787a48c36978dc8aa6ad60
hiwa2719 <hiahmadyan@gmail.com>
2023-01-22 12:09:52 +0330

    updating flow

--- diwar.py
+++ diwar.py
@@ -26,13 +26,59 @@ date = {
 }
 
 hrefs = []
+motors = []
+bodies = []
+failed_requests = []
+counter = start_index = 0
+
+
+async def get_pages(session, link):
+    global counter
+    local_counter = counter
+    counter += 1
+    print('getting link: ', counter)
+    async with session.get(link) as page:
+        text = await page.read()
+        if page.status != 200:
+            failed_requests.append(link[16:])
+            return
+        soup = BeautifulSoup(text.decode('utf-8'), 'html5lib')
+        rows = soup.find_all('div', {'class': 'kt-base-row kt-base-row--large kt-unexpandable-row'})
+
+        for row in rows:
+            title = row.find('p', {'class': 'kt-base-row__title kt-unexpandable-row__title'})
+            value = row.find('p', {'class': 'kt-unexpandable-row__value'})
+
+            if title and value:
+                if 'وضعیت موتور' in title.text and 'نیاز به تعمیر' in value.text:
+                    motors.append(link)
+                elif 'وضعیت بدنه' in title.text and 'تصادفی' in value.text:
+                    bodies.append(link)
+
+    print('finished link: ', local_counter + 1)
+
+
+def asyncro(hrefs):
+    async def async_requests(loop):
+        global counter
+        tasks = list()
+        async with aiohttp.ClientSession(loop=loop) as session:
+            for href in hrefs:
+                link = f'https://divar.ir{href}'
+                tasks.append(get_pages(session, link))
+            grouped_tasks = asyncio.gather(*tasks)
+            return await grouped_tasks
+
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(async_requests(loop))
 
 
 def func():
     global hrefs
+    global start_index
     while True:
         soup = BeautifulSoup(browser.page_source, 'lxml')
-        for element in soup.find_all('div', {'class': 'post-card-item'}):
+        for element in soup.find_all('div', {'class': 'post-card-item-af972'}):
             try:
                 date_text = element.find('span', 'kt-post-card__bottom-description kt-text-truncate').text
                 for key in date.keys():
@@ -44,58 +90,27 @@ def func():
                     hrefs.append(href)
             except Exception:
                 pass
-        browser.execute_script("window.scrollBy(0, 500)")
+        browser.execute_script("window.scrollBy(0, 150)")
+        asyncro(hrefs[start_index:])
+        if start_index == len(hrefs):
+            time.sleep(1)
+        else:
+            start_index = len(hrefs)
+
+
+if __name__ == '__main__':
+    func()
+
+    while failed_requests:  # failed requests here will be requested again
+        new, failed_requests = failed_requests, []
+        asyncro(new)
+
+    print(f'Count all Hrefs: {len(hrefs)}')
+    print(f'Count all motors: {len(motors)}')
+    print(f'Count all bodies: {len(bodies)}')
+
+    motors.extend(bodies)
+    for link in motors:
+        browser.execute_script(f'''window.open("{link}", "_blank");''')
         time.sleep(2)
-
-
-func()
-all_count = len(hrefs)
-print(f'Count all Hrefs: {all_count}')
-motors = []
-bodies = []
-counter = 0
-
-
-async def get_pages(session, link):
-    global counter
-    local_counter = counter
-    counter += 1
-    print('getting link: ', counter)
-    async with session.get(link) as page:
-        text = await page.read()
-        soup = BeautifulSoup(text.decode('utf-8'), 'html5lib')
-        rows = soup.find_all('div', {'class': 'kt-base-row kt-base-row--large kt-unexpandable-row'})
-        for row in rows:
-            title = row.find('p', {'class': 'kt-base-row__title kt-unexpandable-row__title'})
-            value = row.find('p', {'class': 'kt-unexpandable-row__value'})
-            if 'وضعیت موتور' in title.text:
-                if 'نیاز به تعمیر' in value.text:
-                    motors.append(link)
-            elif 'وضعیت بدنه' in title.text:
-                if 'تصادفی' in value.text:
-                    bodies.append(link)
-    print('finished link: ', local_counter + 1)
-
-
-async def async_requests(loop):
-    global counter
-    tasks = list()
-    async with aiohttp.ClientSession(loop=loop) as session:
-        for href in hrefs:
-            link = f'https://divar.ir{href}'
-            tasks.append(get_pages(session, link))
-        grouped_tasks = asyncio.gather(*tasks)
-        return await grouped_tasks
-
-
-loop = asyncio.get_event_loop()
-loop.run_until_complete(async_requests(loop))
-
-print(f'Count all motors: {len(motors)}')
-print(f'Count all bodies: {len(bodies)}')
-
-motors.extend(bodies)
-for link in motors:
-    browser.execute_script(f'''window.open("{link}", "_blank");''')
-    time.sleep(2)
 
